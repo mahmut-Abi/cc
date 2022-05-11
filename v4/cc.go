561eb2a8afdfeb8e19d4fd1789364c1258e33990
Jan Mercl <0xjnml@gmail.com>
2022-05-11 14:08:39 +0200

    v4: add _builtin___strncpy_chk

--- v4/cc.go
+++ v4/cc.go
@@ -113,6 +113,7 @@ long double __builtin_fabsl(long double x);
 long long __builtin_llabs(long long j);
 void *__builtin___memcpy_chk(void * dest, void * src, __SIZE_TYPE__ len, __SIZE_TYPE__ destlen);
 void *__builtin___memset_chk(void * dest, int c, __SIZE_TYPE__ len, __SIZE_TYPE__ destlen);
+void *__builtin___strncpy_chk (char *dest, char *src, __SIZE_TYPE__ len, __SIZE_TYPE__ dstlen);
 void *__builtin__strncpy_chk (char *dest, char *src, __SIZE_TYPE__ len, __SIZE_TYPE__ dstlen);
 void __builtin_abort(void);
 
