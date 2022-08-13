ff4d38c75972d3dc7c660dc3a178684ed9dabb88
Lothar May <games@lotharmay.de>
2022-08-13 11:09:59 +0200

    Set error handler to avoid panic if source file does not exist.

--- v4/cpp.go
+++ v4/cpp.go
@@ -751,6 +751,11 @@ func newCPP(cfg *Config, fset *fset, sources []Source, eh errHandler) (*cpp, err
 	}
 	c.tokenizer = newTokenizer(c)
 	c.tok.Ch = eof // Invalidate
+
+	if c.eh == nil {
+		c.eh = func(msg string, args ...interface{}) { fmt.Printf(msg, args...) }
+	}
+
 	return c, nil
 }
 
